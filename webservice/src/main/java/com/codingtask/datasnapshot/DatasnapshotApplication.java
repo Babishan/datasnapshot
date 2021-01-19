18f754b7e34be099da22a3976e2cea66334e750d
Babishan <kjbabi@gmail.com>
2021-01-19 14:50:04 +0100

    Adding Eureka server and client

--- webservice/src/main/java/com/codingtask/datasnapshot/DatasnapshotApplication.java
+++ webservice/src/main/java/com/codingtask/datasnapshot/DatasnapshotApplication.java
@@ -2,8 +2,10 @@ package com.codingtask.datasnapshot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DatasnapshotApplication {
 
 	public static void main(String[] args) {
