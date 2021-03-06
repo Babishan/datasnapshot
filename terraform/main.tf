9b8b0bf780d8ed30152df401e7046a35dcafc96f
Babishan <kjbabi@gmail.com>
2021-03-06 15:19:12 +0100

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "production" {
   associate_public_ip_address = true
   key_name = "gitlab-runner"
   tags = {
-    Name = "new-production-${count.index}"
+    Name = "production-${count.index}"
   }
 }
 
@@ -35,7 +35,7 @@ resource "aws_instance" "development" {
   associate_public_ip_address = true
   key_name = "gitlab-runner"
   tags = {
-    Name = "new-development-${count.index}"
+    Name = "development-${count.index}"
   }
 }
 
